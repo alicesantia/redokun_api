cdfcff2f4b9b419c5a8c902da7ee09d3fb2bab60
Alice <alicesantia@gmail.com>
2022-07-10 18:29:43 +0200

    First commit

--- application/migrations/20220707000000_create_tb_service.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('BASEPATH') OR exit('No directory script access allowed');
-
-class Migration_create_tb_service extends CI_Migration {
-	public function up(){
-		$this->dbforge->create_database($this->config->item('db_database'));
-	}
-}
